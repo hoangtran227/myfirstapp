53d09a2e4826de77ce83f15cb19f9c412911e22d
DEV-01 <hoangtran@tamdongtam.vn>
2018-04-18 16:29:10 +0700

    Styling the app,done

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
@@ -12,4 +13,14 @@ def articles_details(request,slug):
     return render(request,'articles/article_detail.html',{'article' : article})
 @login_required(login_url='/accounts/login/')
 def articles_create(request):
-    return render(request,'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            #save article to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request,'articles/article_create.html',{'form':form})
